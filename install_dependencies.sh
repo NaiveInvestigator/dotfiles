551fe07a51c073f74998ce06038f8128e92818b0
NaiveInvestigator <72654596+NaiveInvestigator@users.noreply.github.com>
2026-01-28 23:48:35 +0600

    feat: added initial test dependencies script

---
+++ install_dependencies.sh
@@ -0,0 +1,5 @@
+echo "Installing Essentials"
+sudo pacman -S cliphist noctalia-shell swww wl-clipboard grimblast slurp tesseract tesseract-data-eng tesseract-data-ben matugen hyprland ddcutil pavucontrol cliphist cava wlsunset brightnessctl imagemagick zen-browser nwg-look nwg-displays neovim gparted kitty
+echo "Installed Essentails"
+echo "Installing nice to have / cool stuff"
+sudo pacman -S easyeffects mpv podman distrobox
