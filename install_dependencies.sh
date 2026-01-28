023e312e2d68a74510110723fc35831da7115b8d
NaiveInvestigator <72654596+NaiveInvestigator@users.noreply.github.com>
2026-01-29 00:13:09 +0600

    feat: added more dependencies script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,5 @@
 echo -e "\e[33mInstalling Essentials\e[0m"
-sudo pacman -S cliphist noctalia-shell swww wl-clipboard grimblast slurp tesseract tesseract-data-eng tesseract-data-ben matugen hyprland ddcutil pavucontrol cliphist cava wlsunset brightnessctl imagemagick zen-browser nwg-look nwg-displays neovim gparted kitty thunar gvfs gvfs-mtp gvfs-smb gvfs-afc gvfs-gphoto2 file-roller thunar-media-tags-plugin thunar-shares-plugin thunar-volman tumbler
+sudo pacman -S cliphist noctalia-shell swww wl-clipboard grimblast slurp tesseract tesseract-data-eng tesseract-data-ben matugen hyprland ddcutil pavucontrol cliphist cava wlsunset brightnessctl imagemagick zen-browser nwg-look nwg-displays neovim gparted kitty thunar gvfs gvfs-nfs gvfs-mtp gvfs-smb gvfs-afc gvfs-gphoto2 file-roller thunar-media-tags-plugin thunar-shares-plugin thunar-volman tumbler zenity btop
 echo -e "\e[32mInstalled Essentials\e[0m"
 echo -e "\e[35mInstalling nice to have / cool stuff\e[0m"
-sudo pacman -S easyeffects mpv podman distrobox heroic-games-launcher-bin protonplus wine btrfs-assistant
+sudo pacman -S easyeffects mpv mpv-mpris yt-dlp podman podman-docker distrobox heroic-games-launcher-bin mangohud wine gamemode gamescope protonplus btrfs-assistant tailscale spoofdpi seahorse foliate filelight
