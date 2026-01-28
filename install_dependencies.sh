3331a3325459a4bc7be1e8f3b23d05dff2a98b8f
NaiveInvestigator <72654596+NaiveInvestigator@users.noreply.github.com>
2026-01-29 00:03:04 +0600

    feat: added colors and more dependencies script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,5 @@
-echo "Installing Essentials"
+echo -e "\e[33mInstalling Essentials\e[0m"
 sudo pacman -S cliphist noctalia-shell swww wl-clipboard grimblast slurp tesseract tesseract-data-eng tesseract-data-ben matugen hyprland ddcutil pavucontrol cliphist cava wlsunset brightnessctl imagemagick zen-browser nwg-look nwg-displays neovim gparted kitty thunar gvfs gvfs-mtp gvfs-smb gvfs-afc gvfs-gphoto2 file-roller thunar-media-tags-plugin thunar-shares-plugin thunar-volman tumbler
-echo "Installed Essentails"
-echo "Installing nice to have / cool stuff"
-sudo pacman -S easyeffects mpv podman distrobox
+echo -e "\e[32mInstalled Essentials\e[0m"
+echo -e "\e[35mInstalling nice to have / cool stuff\e[0m"
+sudo pacman -S easyeffects mpv podman distrobox heroic-games-launcher-bin protonplus wine btrfs-assistant
