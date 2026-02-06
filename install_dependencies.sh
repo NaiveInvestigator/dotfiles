120b44303b441c447ea10a46b56f05db2841bc0b
NaiveInvestigator <72654596+NaiveInvestigator@users.noreply.github.com>
2026-02-07 02:33:31 +0600

    feat: added hyprpicker to dependencies script for freeze screenshot

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,5 @@
 echo -e "\e[33mInstalling Essentials\e[0m"
-sudo pacman -S cliphist noctalia-shell swww wl-clipboard grimblast slurp tesseract tesseract-data-eng tesseract-data-ben matugen hyprland ddcutil pavucontrol cliphist cava wlsunset brightnessctl imagemagick zen-browser nwg-look nwg-displays neovim gparted kitty thunar gvfs gvfs-nfs gvfs-mtp gvfs-smb gvfs-afc gvfs-gphoto2 file-roller thunar-media-tags-plugin thunar-shares-plugin thunar-volman tumbler zenity btop fastfetch
+sudo pacman -S cliphist noctalia-shell swww wl-clipboard grimblast grim slurp hyprpicker tesseract tesseract-data-eng tesseract-data-ben matugen hyprland ddcutil pavucontrol cliphist cava wlsunset brightnessctl imagemagick zen-browser nwg-look nwg-displays neovim gparted kitty thunar gvfs gvfs-nfs gvfs-mtp gvfs-smb gvfs-afc gvfs-gphoto2 file-roller thunar-media-tags-plugin thunar-shares-plugin thunar-volman tumbler zenity btop fastfetch
 echo -e "\e[32mInstalled Essentials\e[0m"
 echo -e "\e[35mInstalling nice to have / cool stuff\e[0m"
 sudo pacman -S easyeffects mpv mpv-mpris yt-dlp podman podman-docker distrobox heroic-games-launcher-bin mangohud wine gamemode gamescope protonplus btrfs-assistant tailscale spoofdpi seahorse foliate filelight qbittorrent
